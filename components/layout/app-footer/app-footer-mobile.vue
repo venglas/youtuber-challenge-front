b3472002192a5b86b265d9838c03cf83ced5f81f
Bartek <bartek.bober.koder@gmail.com>
2020-10-30 22:56:57 +0100

    add componen for footer link

---
+++ components/layout/app-footer/app-footer-mobile.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="app-footer--mobile">
+    <footer-link link-to="home">
+      <i-home class="icon" />
+    </footer-link>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/variables.scss';
+.app-footer {
+  &--mobile {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: $S_mobile_header_height;
+    background-color: $C_light-header;
+  }
+}
+</style>
---
+++ components/layout/app-footer/footer-link.vue
@@ -0,0 +1,32 @@
+<template>
+  <nuxt-link :to="link" class="footer-link">
+    <slot />
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import { computed } from '@vue/composition-api'
+export default {
+  props: {
+    linkTo: { type: String, default: '' }
+  },
+  setup (props) {
+    const link = computed((): String => `/${props.linkTo}`)
+
+    return {
+      link
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/space';
+
+.footer-link {
+  .icon {
+    @include space-p-med;
+    width: 70px
+  }
+}
+</style>
