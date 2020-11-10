525d38ca88ac99799339c35087d9ce7ec9a68576
Bartek <bartek.bober.koder@gmail.com>
2020-11-10 20:42:51 +0100

    add displaying info bout wrong confirmation code

--- components/layout/forms/signup-confirmation-form.vue
+++ components/layout/forms/signup-confirmation-form.vue
@@ -2,7 +2,9 @@
   <form @submit.prevent="confirmation()">
     <h2>Retype your confirmation code form email.</h2>
     <form-input name="confirm" type="text" data-mutation-entry="userSignup/setVerificationCode" required />
-
+    <p v-if="confirmationError" class="error">
+      Confirmation code is wrong.
+    </p>
     <button-base type="submit">
       Confirm
     </button-base>
@@ -10,12 +12,13 @@
 </template>
 
 <script>
-import { computed, onMounted } from '@nuxtjs/composition-api'
+import { computed, onMounted, ref } from '@nuxtjs/composition-api'
 
 export default {
   setup (_, { root: { $store, $axios } }) {
     const email = computed(() => $store.state.userSignup.email)
     const verificationCode = computed(() => $store.state.userSignup.verificationCode)
+    const confirmationError = ref(false)
 
     onMounted(() => {
       $axios.get('signup/confirmation-code', { params: { email } }).then((res) => {
@@ -26,16 +29,27 @@ export default {
     const confirmation = () => {
       if (verificationCode.value === $store.state.userSignup.verificationCodeApi) {
         $store.commit('userSignup/setSignupStep', 2)
+      } else {
+        confirmationError.value = true
       }
     }
 
     return {
-      confirmation
+      confirmation,
+      confirmationError
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/assets/scss/mixins';
+@import '@/assets/scss/variables';
 
+.error {
+  font-size: 1.5rem;
+  text-align: center;
+  margin-bottom: 1.5rem;
+  color: $C_base-error-font;
+}
 </style>
